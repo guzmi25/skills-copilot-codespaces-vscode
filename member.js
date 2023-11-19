24a5752a4f73ebb67d8d61e137d59be17f2c8e1e
guzmi25 <76704022+guzmi25@users.noreply.github.com>
2023-11-19 21:41:18 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,12 @@
+function skillsMember() {
+    var member = document.getElementById("member");
+    member.style.display = "block";
+    var skills = document.getElementById("skills");
+    skills.style.display = "none";
+    var contact = document.getElementById("contact");
+    contact.style.display = "none";
+    var about = document.getElementById("about");
+    about.style.display = "none";
+    var home = document.getElementById("home");
+    home.style.display = "none";
+}
